8aea67c67a7dd6f08fbeab9c3eee674d3450064e
Demian Neidetcher <demian0311@gmail.com>
2012-02-09 22:13:35 -0700

    comments

--- src/test/scala/com/neidetcher/scalabox/sfti/chapter1/TheBasics.scala
+++ src/test/scala/com/neidetcher/scalabox/sfti/chapter1/TheBasics.scala
@@ -5,14 +5,12 @@ import org.junit.Assert._
 
 class TheBasics {
 
-    @Test
-    def one_1() {
+    @Test def one_1() {
         val b = 8 * 5 + 2
         assertEquals(42, b)
 
         val expected: Double = 21
-        // TODO: not sure what's wrong here
-        // assertEquals(expected, 0.5 * b)
+        //assertEquals(expected, (0.5 * b).toDouble)
 
         val result = "Hello, " + b
         assertEquals("Hello, 42", result)
@@ -21,8 +19,7 @@ class TheBasics {
         assertEquals("HELLO, 42", result.toUpperCase())
     }
 
-    @Test
-    def one_2 {
+    @Test def one_2 {
         val answer = 8 * 5 + 2
         assertEquals(42, answer)
 
@@ -41,28 +38,27 @@ class TheBasics {
         assertNull(message)
     }
 
-    @Test
-    def one_3 {
+    /**
+     * scala gives more string stuff in StringOps
+     * http://www.scala-lang.org/api/current/scala/collection/immutable/StringOps.html
+     *
+     * this magic is from RichInt
+     * http://www.scala-lang.org/api/current/index.html#scala.runtime.RichInt
+     */
+    @Test def one_3 {
         assertEquals("1", 1.toString)
 
         // TODO: why doesn't this work?
         //assertEquals(Range(1, 2, 3), 1.to(3))
-
         assertEquals("lo", "Hello".intersect("World")) // yields "lo"
 
-        // scala gives more string stuff in StringOps
-        // http://www.scala-lang.org/api/current/scala/collection/immutable/StringOps.html
-
-        // this magic is from RichInt
-        // http://www.scala-lang.org/api/current/index.html#scala.runtime.RichInt
         1.to(5)
 
         assertEquals(99, 99.44.toInt)
         assertEquals('c', 99.toChar)
     }
 
-    @Test
-    def one_4 {
+    @Test def one_4 {
         val answer = 8 * 5 + 2
         assertEquals(42, answer)
 
@@ -74,6 +70,31 @@ class TheBasics {
 
         val x: BigInt = 1234567890
         val result = x * x * x // in Java you'd have to use .multiply
+    }
+
+    /**
+     * http://www.scala-lang.org/api/current/index.html#scala.Math$
+     *
+     * http://www.scala-lang.org/api/current/index.html#scala.math.BigInt
+     */
+    @Test def one_5 {
+        import math._
+
+        assertEquals(3, sqrt(9).toInt)
+        assertEquals(16, pow(2, 4).toInt)
+        assertEquals(3, min(3, Pi).toInt)
+
+        assertEquals("Helo", "Hello".distinct)
+    }
+
+    /**
+     * The apply method
+     */
+    @Test def one_6 {
+        assertEquals('o', "Hello"(4))
+        assertEquals(BigInt("1234567890"), BigInt.apply("1234567890"))
+        // BigInt has a companion object (a static) with an apply method
+        // this is the idiomatic way to instantiate new classes in scala
     }
 
     def java_like_statements() {
